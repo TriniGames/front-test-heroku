3e1fd481b3135b15970bd079fd2f551a717b54d5
Gonzalo Corsanigo <gonzalocor_10@hotmail.com>
2021-11-29 20:18:28 -0300

    fix server 2

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/front-end-vina-alta'));
+app.use(express.static(__dirname + '/dist/front-end-vina-alta'));
+// app.use(express.static('./dist/front-end-vina-alta'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/front-end-vina-alta/' }),
+    // res.sendFile('index.html', { root: 'dist/front-end-vina-alta/' }),
+    res.sendFile(path.join(__dirname + '/dist/front-end-vina-alta/index.html'))
 );
 
 // Start the app by listening on the default Heroku port
